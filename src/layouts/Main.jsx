e44ffb23963293bb0be2a5e49ce73367bd742b83
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-06-07 16:45:56 +0600

    added navbar and footer

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+import Footer from "../pages/Shared/Footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="pt-24 min-h-[75vh]">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
