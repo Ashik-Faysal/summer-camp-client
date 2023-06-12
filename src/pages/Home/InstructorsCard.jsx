f466fcb91e20e6eb9993ff7cbe8bbcda532906fc
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-06-12 09:46:31 +0600

    added instructor section on home page

---
+++ src/pages/Home/InstructorsCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const InstructorsCard = ({ instructor }) => {
+    console.log(instructor);
+    const { name, image, email, students} = 
+        instructor;
+  return (
+    <div className="card w-full bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img
+          src={image}
+          alt="Shoes"
+          className="rounded-xl"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+              <h2 className="card-title">{name}</h2>
+              <p>{email}</p>
+              <p>Num_of_Students:{students}</p>
+        <div className="card-actions">
+          <button className="btn btn-primary">Instructors Details</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InstructorsCard;
--- src/pages/Home/PopularInstructors.jsx
+++ src/pages/Home/PopularInstructors.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import InstructorsCard from './InstructorsCard';
 
 const PopularInstructors = () => {
+  const [instructors, setInstructors] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/instructor")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setInstructors(data); // Store the fetched data in state
+      });
+  }, []);
     return (
-      <div>
-        <h1 className="text-5xl font-bold text-orange-500 my-8 text-center">
-          Popular Instructors
-        </h1>
-      </div>
+      <>
+        <div>
+          <h1 className="text-5xl font-bold text-orange-500 my-8 text-center">
+            Popular Instructors
+          </h1>
+        </div>
+        <div className="grid md:grid-cols-3 gap-3">
+          {instructors.map((instructor) => (
+            <InstructorsCard key={instructor._id} instructor={instructor} />
+          ))}
+        </div>
+      </>
     );
 };
 
