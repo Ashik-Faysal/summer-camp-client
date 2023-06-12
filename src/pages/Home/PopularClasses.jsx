35e36c15199c01175771b138d666174d287a8b51
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-06-12 10:58:56 +0600

    added private route

--- src/pages/Home/PopularClasses.jsx
+++ src/pages/Home/PopularClasses.jsx
@@ -4,7 +4,7 @@ const PopularClasses = () => {
   const [classes, setClasses] = useState([]);
 
   useEffect(() => {
-    fetch("popularClass.json")
+    fetch("http://localhost:5000/classes")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -16,8 +16,10 @@ const PopularClasses = () => {
   }, []);
 
   return (
-      <>
-          <h1 className="text-5xl font-bold text-orange-500 my-8 text-center">Popular Class</h1>
+    <>
+      <h1 className="text-5xl font-bold text-orange-500 my-8 text-center">
+        Popular Class
+      </h1>
       <div className="grid md:grid-cols-3 gap-3">
         {classes &&
           classes.map((item) => (
@@ -26,17 +28,17 @@ const PopularClasses = () => {
               className="card w-full bg-orange-400-100 shadow-xl"
             >
               <figure className="px-10 pt-10">
-                <img
-                  src={item.classPicture}
-                  alt="Class"
-                  className="rounded-xl"
-                />
+                <img src={item.image} alt="Class" className="rounded-xl" />
               </figure>
               <div className="card-body items-center text-center">
-                <h2 className="card-title">{item.className}</h2>
-                <p>Number Of Students: {item.NumberOfStudents}</p>
+                <h2 className="card-title">{item.name}</h2>
+                <p>Instructor Name : {item.instructorName}</p>
+                <p>Available Sets : {item.availableSeats}</p>
+                <p>CoursePrice : {item.price}$</p>
                 <div className="card-actions">
-                  <button className="btn btn-primary">Details</button>
+                  {item.availableSeats && (
+                    <button className="btn btn-primary">Select</button>
+                  )}
                 </div>
               </div>
             </div>
