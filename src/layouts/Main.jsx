ff853ac4c35b3e79cb55cb0dc459e86d766fb9fc
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-06-13 13:20:48 +0600

    noHeader Footer for Navbar and footer components

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar/NavBar";
 import Footer from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
+  const location = useLocation();
+  const noHeaderFooter =
+    location.pathname.includes("login") || location.pathname.includes("signup");
   return (
     <div>
-      <Navbar />
+      {noHeaderFooter || <Navbar />}
       <div className="pt-24 min-h-[75vh]">
         <Outlet />
       </div>
-      <Footer />
+      {noHeaderFooter || <Footer/>}
     </div>
   );
 };
